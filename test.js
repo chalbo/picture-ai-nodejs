70dae7b25cb0d4500681f2053e08b74c769b97df
张波 <zhangbo2@sugon.com>
2020-09-03 17:29:41 +0800

    添加视频测试

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const cv = require('opencv4nodejs');
-const { darkNet } = require('./app/distributed/openCv/code/darkNet');
+const { DarkNet } = require('./app/distributed/openCv/code/darkNet');
 // loadModel()
 const darknetPath = "/app/distributed/openCv/data/struct/Flag_Recon/";
 const cfgFile = path.join(__dirname, darknetPath + "Project_Obj365_test.cfg");
@@ -9,5 +9,7 @@ const labelsFile = path.join(__dirname, darknetPath + "Project_Obj365.names");
 
 const imageUrl = '/app/distributed/openCv/data/struct/images/444.jpeg';
 const img = cv.imread(path.join(__dirname, imageUrl));
-const dn = new darkNet({ cfgFile, weightsFile, labelsFile, minConfidence: 0.5, nmsThreshold: 0.3 });
-cv.imshowWait("Darknet YOLO Object Detection", dn.recImage(img));
+const dn = new DarkNet({ cfgFile, weightsFile, labelsFile, minConfidence: 0.5, nmsThreshold: 0.3 });
+//cv.imshowWait("Darknet YOLO Object Detection", dn.runVideo(img));
+const videoUrl = path.join(__dirname, '/app/distributed/openCv/data/person.mp4');
+dn.runVideo(videoUrl);
